00aba6f57f721ba3f0bffa48bb9c088761921780
MrMirka <aleshin.dev@gmail.com>
2024-02-12 14:59:27 -0300

    finak

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import './style.css'
-
 import * as THREE from 'three';
 import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
@@ -9,104 +8,109 @@ import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
 import modelPath from './src/models/model.gltf?url';
 import fragmentShader from './src/shaders/fragmant.frag'
 import vertexShader from './src/shaders/vertex.vert'
-import { createTextTexture } from './utils';
+import { addTextBlock, createTextTextureFromHtml, getObjectDimention, getScreenDimention } from './utils';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const BlackWhiteShader = {
+        uniforms: {
+            "tDiffuse": { value: null },
+            "uTime": { value: 0.0 },
+            "uMouse": { value: new THREE.Vector2(-1, -1) },
+            "uAspectRatio": { value: null },
+
+        },
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+    };
 
 
-const BlackWhiteShader = {
-    uniforms: {
-        "tDiffuse": { value: null },
-        "uTime": { value: 0.0 },
-        "uMouse": { value: new THREE.Vector2(-1, -1) },
-        "uAspectRatio": { value: null },
-        
-    },
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader
-};
+    let monkey = null
+
+    const gltfLoader = new GLTFLoader()
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 2.6
+    scene.add(camera)
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.physicallyCorrectLights = true
+    renderer.toneMapping = THREE.ACESFilmicToneMapping
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap
+    renderer.shadowMap.enabled = true
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap
+    renderer.logarithmicDepthBuffer = true
+    renderer.autoClear = true
+    document.body.appendChild(renderer.domElement);
 
 
-const gltfLoader = new GLTFLoader()
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 2.6
-scene.add(camera)
-
-const renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setPixelRatio( window.devicePixelRatio )
-renderer.physicallyCorrectLights = true
-renderer.toneMapping = THREE.ACESFilmicToneMapping
-renderer.setSize( window.innerWidth, window.innerHeight )
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.logarithmicDepthBuffer = true
-renderer.autoClear = true
-document.body.appendChild( renderer.domElement );
-
-const aspectRatio = renderer.domElement.width / renderer.domElement.height;
 
 
-//light
-const pointLight = new THREE.PointLight( 0xffffff, 161, 100 )
- pointLight.position.set( 1, 1, 1 )
- pointLight.castShadow = true
- pointLight.shadow.normalBias = 0.05
- scene.add( pointLight )
+    //light
+    const pointLight = new THREE.PointLight(0xffffff, 161, 100)
+    pointLight.position.set(1, 1, 1)
+    pointLight.castShadow = true
+    pointLight.shadow.normalBias = 0.05
+    scene.add(pointLight)
+
+    //Model
+    gltfLoader.load(modelPath, gltf => {
+        monkey = gltf.scene
+        monkey.position.x = 1
+        scene.add(monkey)
+    })
+
+    // Инициализация EffectComposer
+    const composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
+
+    // Добавление кастомного шейдера как ShaderPass
+    const blackWhitePass = new ShaderPass(BlackWhiteShader);
+    composer.addPass(blackWhitePass);
+
+    blackWhitePass.uniforms.uAspectRatio.value = window.innerWidth / window.innerHeight;
 
 
-gltfLoader.load(modelPath, gltf => {
-    scene.add(gltf.scene)
+    document.addEventListener('mousemove', (e) => {
+        const x = (e.clientX / window.innerWidth) * 2 - 1;
+        const y = -(e.clientY / window.innerHeight) * 2 + 1;
+        blackWhitePass.uniforms.uMouse.value.set(x, y);
+    });
+
+    window.addEventListener('resize', function () {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        blackWhitePass.uniforms.uAspectRatio.value = window.innerWidth / window.innerHeight;
+    });
+
+    
+    const scrollGroup = new THREE.Group()
+    const tBlock1 = addTextBlock(0,0,'hello', camera)
+    const tBlock2 = addTextBlock(0,0.5,'testText', camera)
+    scrollGroup.add(tBlock1)
+    scrollGroup.add(tBlock2)
+    scene.add(scrollGroup)
+
+
+    const scrollSpeed = 0.1;
+    document.addEventListener('wheel', function (event) {
+        const deltaY = event.deltaY * 0.01;
+        scrollGroup.position.y += deltaY * scrollSpeed;
+        if(monkey) {
+            monkey.rotation.y +=deltaY * scrollSpeed * 0.1;
+        }
+    });
+
+
+    animate()
+    function animate() {
+        requestAnimationFrame(animate)
+        blackWhitePass.uniforms.uTime.value = performance.now() / 1000;
+        composer.render();
+    }
 })
 
-// Инициализация EffectComposer
-const composer = new EffectComposer(renderer);
-composer.addPass(new RenderPass(scene, camera));
-
-// Добавление кастомного шейдера как ShaderPass
-const blackWhitePass = new ShaderPass(BlackWhiteShader);
-composer.addPass(blackWhitePass);
-
-blackWhitePass.uniforms.uAspectRatio.value = window.innerWidth / window.innerHeight;
-
-
-
-
-//renderer.render(scene, camera)
-document.addEventListener('mousemove', (e) => {
-    const x = (e.clientX / window.innerWidth) * 2 - 1;
-    const y = -(e.clientY / window.innerHeight) * 2 + 1;
-    blackWhitePass.uniforms.uMouse.value.set(x, y);
-});
-
-window.addEventListener('resize', function() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    blackWhitePass.uniforms.uAspectRatio.value = window.innerWidth / window.innerHeight;
-});
-
-
-const textTexture = createTextTexture('Привет BETBOOM', 512, 68)
-const textureWidth = textTexture.image.width;
-const textureHeight = textTexture.image.height;
-const material = new THREE.MeshBasicMaterial({ 
-    map: textTexture,
-    transparent: true, // Важно для прозрачности
-    alphaTest: 0.5 // Убираем полностью прозрачные части, чтобы избежать проблем с сортировкой
-});
-const geo = new THREE.PlaneGeometry(textureWidth * 0.005,textureHeight * 0.005)
-const mesh = new THREE.Mesh(geo, material)
-mesh.position.x = -1.8
-mesh.position.y = 1
-scene.add(mesh)
-
-
-animate()
-function animate(){
-    //renderer.render(scene, camera)
-    requestAnimationFrame(animate)
-    blackWhitePass.uniforms.uTime.value = performance.now() / 1000;
-    composer.render();
-} 
 
--- utils.js
+++ utils.js
@@ -2,9 +2,9 @@ import * as THREE from 'three';
 export function createTextTexture(text, width, fontSize) {
     var canvas = document.createElement('canvas');
     var context = canvas.getContext('2d');
-    
+
     canvas.width = width;
-    canvas.height = 200; // Вы можете настроить этот размер
+    canvas.height = 200;
     context.font = `bold ${fontSize}px Arial`;
     context.fillStyle = "rgba(255, 134, 23, 1)";
 
@@ -33,9 +33,108 @@ export function createTextTexture(text, width, fontSize) {
 
     wrapText(context, text, 0, fontSize, canvas.width, fontSize * 1.2);
 
-    // Создаем текстуру, указывая, что фон должен быть прозрачным
+
     var texture = new THREE.CanvasTexture(canvas);
-    texture.needsUpdate = true; // Обеспечиваем обновление текстуры
+    texture.needsUpdate = true;
 
     return texture;
+}
+
+export function createTextTextureFromHtml(obj) {
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    const htmlObj = document.getElementById(obj)
+
+    const width = htmlObj.offsetWidth;
+    const height = htmlObj.offsetHeight;
+
+    // Получаем текст
+    const text = htmlObj.innerText;
+    const computedStyle = getComputedStyle(htmlObj);
+    const fontFamily = computedStyle.fontFamily;
+    const color = computedStyle.color;
+    const fontSize = parseInt(computedStyle.fontSize) * window.devicePixelRatio;
+
+
+    canvas.width = width * window.devicePixelRatio;
+    canvas.height = (height + 24) * window.devicePixelRatio;
+    
+    context.font = `${fontSize}px ${fontFamily}`;
+    //context.fillStyle = "rgba(255, 255, 255, 1)";
+    context.fillStyle = color;
+    context.imageSmoothingEnabled = true;
+    
+
+    // Очищаем canvas с прозрачным фоном
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    //Функция переноса строк
+    function wrapText(context, text, x, y, maxWidth, lineHeight) {
+        var words = text.split(' ');
+        var line = '';
+
+        for (var n = 0; n < words.length; n++) {
+            var testLine = line + words[n] + ' ';
+            var metrics = context.measureText(testLine);
+            var testWidth = metrics.width;
+            if (testWidth > maxWidth && n > 0) {
+                context.fillText(line, x, y);
+                line = words[n] + ' ';
+                y += lineHeight;
+            } else {
+                line = testLine;
+            }
+        }
+        context.fillText(line, x, y);
+    }
+
+    wrapText(context, text, 0, fontSize, canvas.width, fontSize * 1.2);
+
+
+    var texture = new THREE.CanvasTexture(canvas);
+    texture.needsUpdate = true;
+
+    return texture;
+}
+
+/**
+ * Получаем размер видимой области камеры в относительных еденицах
+ * @param {*} camera 
+ * @returns 
+ */
+export function getScreenDimention(camera) {
+    let dimention = { width: 0, height: 0 }
+    const vFOV = THREE.MathUtils.degToRad(camera.fov);
+    const height = 2 * Math.tan(vFOV / 2) * camera.position.z;
+    const width = height * (window.innerWidth / window.innerHeight);
+    dimention.width = width
+    dimention.height = height
+    return dimention
+}
+
+export function getObjectDimention(object) {
+    object.computeBoundingBox();
+    const widht = object.boundingBox.max.x - object.boundingBox.min.x;
+    const height = object.boundingBox.max.y - object.boundingBox.min.y;
+    return { widht, height }
+}
+
+export function addTextBlock(x,y,obj, camera){
+    const { width, height } = getScreenDimention(camera);
+    const shiftSize = width / window.innerWidth;
+    const textTexture = createTextTextureFromHtml(obj)
+    const textureWidth = textTexture.image.width;
+    const textureHeight = textTexture.image.height;
+    const material = new THREE.MeshBasicMaterial({
+        map: textTexture,
+        transparent: true,
+        alphaTest: 0.5 
+    });
+    const geo = new THREE.PlaneGeometry(textureWidth * shiftSize * 0.5, textureHeight * shiftSize * 0.5)
+    const mesh = new THREE.Mesh(geo, material)
+    const text = getObjectDimention(geo)
+    mesh.position.x = (-width / 2 + text.widht / 2) + x
+    mesh.position.y = (height / 2 - text.height / 2) - y
+    mesh.position.z = 0
+    return mesh
 }
