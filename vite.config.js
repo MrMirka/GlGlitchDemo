117ffaebda97c0a116526234147204c3af033474
MrMirka <aleshin.dev@gmail.com>
2024-02-12 15:24:04 -0300

    fx

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import string from 'vite-plugin-string';
 
 export default defineConfig({
+  base: '/WEbGlDemo/',
   plugins: [
     string({ include: ['**/*.frag', '**/*.vert'] }),
   ],
